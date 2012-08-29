dd1774424925231c016d9362d96fdf56be33c68b
Jun Kikuchi <kikuchi@bonnou.com>
2012-08-29 23:03:13 +0900

    動作確認用クライアントをターゲットに追加

---
+++ AppEngineChannelClientExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AppEngineChannelClientExample
+//
+//  Created by 菊池 淳 on 12/08/29.
+//  Copyright (c) 2012年 Jun Kikuchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AppEngineChannelClientExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AppEngineChannelClientExample
+//
+//  Created by 菊池 淳 on 12/08/29.
+//  Copyright (c) 2012年 Jun Kikuchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
