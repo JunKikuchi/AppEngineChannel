3d1b08dc7d89a5f4796e20267996544178344e5f
Jun Kikuchi <kikuchi@bonnou.com>
2012-08-29 22:51:19 +0900

    Initial Commit

---
+++ AppEngineChannel/AppEngineChannel.h
@@ -0,0 +1,13 @@
+//
+//  AppEngineChannel.h
+//  AppEngineChannel
+//
+//  Created by 菊池 淳 on 12/08/29.
+//  Copyright (c) 2012年 Jun Kikuchi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppEngineChannel : NSObject
+
+@end
---
+++ AppEngineChannel/AppEngineChannel.m
@@ -0,0 +1,13 @@
+//
+//  AppEngineChannel.m
+//  AppEngineChannel
+//
+//  Created by 菊池 淳 on 12/08/29.
+//  Copyright (c) 2012年 Jun Kikuchi. All rights reserved.
+//
+
+#import "AppEngineChannel.h"
+
+@implementation AppEngineChannel
+
+@end
