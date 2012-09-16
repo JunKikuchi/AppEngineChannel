eeb331429a4fc131422fd832e70629c3d64ad7f4
Jun Kikuchi <kikuchi@bonnou.com>
2012-09-16 21:29:07 +0900

    fixed main.js

--- AppEngineChannelServerExample/static/main.js
+++ AppEngineChannelServerExample/static/main.js
@@ -1,40 +1,46 @@
 $(function(){
   $('form').submit(function() {
-    message = $(this).find('#message').val();
+    var message = $(this).find('#message').val();
 
     $.post('/channel/echo', {message: message});
     return false;
   });
 
   var render_message = function(client_id, message) {
-    $('#messages').append('<dt>' + client_id + '</dt>');
-    $('#messages').append('<dd>' + message + '</dd>');
+    $('#messages')
+      .append('<dt>' + client_id + '</dt>')
+      .append('<dd>' + message + '</dd>');
+  };
+
+  var onopen = function() {
+    console.log('channel opened');
+  };
+
+  var onmessage = function(data) {
+    console.log('channel data:');
+    console.log(data);
+
+    var data = $.parseJSON(data.data);
+
+    render_message(data.client_id, data.message);
+  };
+
+  var onerror = function(error) {
+    console.log('channel error: code=' + error.code + ' desc=' + error.description);
+  };
+
+  var onclose = function() {
+    console.log('channel closeed');
   };
 
   var connect = function(data) {
-    channel = new goog.appengine.Channel(data.token);
-    socket = channel.open();
-
-    socket.onopen = function() {
-      console.log('channel opened');
-    };
-
-    socket.onmessage = function(data) {
-      console.log('channel data:');
-      console.log(data);
-
-      var data = $.parseJSON(data.data);
-
-      render_message(data.client_id, data.message);
-    };
-
-    socket.onerror = function(error) {
-      console.log('channel error: code=' + error.code + ' desc=' + error.description);
-    };
-
-    socket.onclose = function() {
-      console.log('channel closeed');
-    };
+    var channel = new goog.appengine.Channel(data.token);
+    var socket = channel.open({
+      onopen: onopen,
+      onmessage: onmessage,
+      onerror: onerror,
+      onclose: onclose
+    });
   };
 
   $.getJSON('/channel', {client_id: client_id}, connect);
